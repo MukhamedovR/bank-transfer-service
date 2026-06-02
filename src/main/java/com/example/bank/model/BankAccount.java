0bffca59ee02d6d193038a10a899c2489905dced
MukhamedovR <255413916+MukhamedovR@users.noreply.github.com>
2026-06-02 12:19:59 +0300

    Update BankAccount.java

--- src/main/java/com/example/bank/model/BankAccount.java
+++ src/main/java/com/example/bank/model/BankAccount.java
@@ -4,27 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import java.math.BigDecimal;
 
-@Entity // Эта аннотация говорит Spring, что на основе этого класса нужно создать таблицу в базе данных
+@Entity
 public class BankAccount {
 
-    @Id // Указывает, что это поле — уникальный идентификатор (Primary Key)
+    @Id 
     private String id;
     private String accountHolder;
     private BigDecimal balance;
     private String currency;
 
-    // Пустой конструктор (обязателен для работы с базой данных)
     public BankAccount() {}
 
-    // Конструктор для удобного создания нового счета
     public BankAccount(String id, String accountHolder, String currency) {
         this.id = id;
         this.accountHolder = accountHolder;
-        this.balance = BigDecimal.ZERO; // По ТЗ начальный баланс всегда равен 0
+        this.balance = BigDecimal.ZERO;
         this.currency = currency;
     }
 
-    // Геттеры и сеттеры (нужны, чтобы Spring мог читать и изменять данные)
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
 
