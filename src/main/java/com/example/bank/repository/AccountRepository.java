20350db1ace64180c351d9f6f7b04e8a4d83fba0
MukhamedovR <255413916+MukhamedovR@users.noreply.github.com>
2026-06-02 12:21:27 +0300

    Update AccountRepository.java

--- src/main/java/com/example/bank/repository/AccountRepository.java
+++ src/main/java/com/example/bank/repository/AccountRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-// JpaRepository<BankAccount, String> означает: работаем со счетами, у которых ID — это String
 public interface AccountRepository extends JpaRepository<BankAccount, String> {
 }
